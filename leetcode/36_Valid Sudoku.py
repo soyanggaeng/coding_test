eb5693c7a9d458c6d8f525d2ecba62a8178b8ade
소연경 <102569551+soyanggaeng@users.noreply.github.com>
2024-03-12 15:52:37 +0900

    Create 36_Valid Sudoku.py

---
+++ leetcode/36_Valid Sudoku.py
@@ -0,0 +1,71 @@
+# Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+# Each row must contain the digits 1-9 without repetition.
+# Each column must contain the digits 1-9 without repetition.
+# Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+# Note:
+
+# A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+# Only the filled cells need to be validated according to the mentioned rules.
+ 
+
+# Example 1:
+
+
+# Input: board = 
+# [["5","3",".",".","7",".",".",".","."]
+# ,["6",".",".","1","9","5",".",".","."]
+# ,[".","9","8",".",".",".",".","6","."]
+# ,["8",".",".",".","6",".",".",".","3"]
+# ,["4",".",".","8",".","3",".",".","1"]
+# ,["7",".",".",".","2",".",".",".","6"]
+# ,[".","6",".",".",".",".","2","8","."]
+# ,[".",".",".","4","1","9",".",".","5"]
+# ,[".",".",".",".","8",".",".","7","9"]]
+# Output: true
+# Example 2:
+
+# Input: board = 
+# [["8","3",".",".","7",".",".",".","."]
+# ,["6",".",".","1","9","5",".",".","."]
+# ,[".","9","8",".",".",".",".","6","."]
+# ,["8",".",".",".","6",".",".",".","3"]
+# ,["4",".",".","8",".","3",".",".","1"]
+# ,["7",".",".",".","2",".",".",".","6"]
+# ,[".","6",".",".",".",".","2","8","."]
+# ,[".",".",".","4","1","9",".",".","5"]
+# ,[".",".",".",".","8",".",".","7","9"]]
+# Output: false
+# Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+ 
+
+# Constraints:
+
+# board.length == 9
+# board[i].length == 9
+# board[i][j] is a digit 1-9 or '.'.
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        # Each row must contain the digits 1-9 without repetition.
+        for row in board:
+            if not self.isUnique(row):
+                return False
+        
+        # Each column must contain the digits 1-9 without repetition.
+        for col in range(9):
+            column = [board[row][col] for row in range(9)]
+            if not self.isUnique(column):
+                return False
+        
+        for startRow in range(0, 9, 3):
+            for startCol in range(0, 9, 3):
+                sub_box = [board[row][col] for row in range(startRow, startRow+3) for col in range(startCol, startCol+3)]
+                if not self.isUnique(sub_box):
+                    return False
+        
+        return True
+
+    def isUnique(self, elements: List[str]) -> bool:
+        elements = [elem for elem in elements if elem != '.']
+        return len(elements) == len(set(elements))
