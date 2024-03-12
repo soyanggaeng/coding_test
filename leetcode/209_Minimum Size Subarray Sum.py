13f7a21d4432179d64f71df52d648abd90bd2c2b
소연경 <102569551+soyanggaeng@users.noreply.github.com>
2024-03-12 15:06:54 +0900

    Create 209_Minimum Size Subarray Sum.py

---
+++ leetcode/209_Minimum Size Subarray Sum.py
@@ -0,0 +1,45 @@
+# Given an array of positive integers nums and a positive integer target, return the minimal length of a 
+# subarray
+#  whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
+
+ 
+
+# Example 1:
+
+# Input: target = 7, nums = [2,3,1,2,4,3]
+# Output: 2
+# Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+# Example 2:
+
+# Input: target = 4, nums = [1,4,4]
+# Output: 1
+# Example 3:
+
+# Input: target = 11, nums = [1,1,1,1,1,1,1,1]
+# Output: 0
+ 
+
+# Constraints:
+
+# 1 <= target <= 109
+# 1 <= nums.length <= 105
+# 1 <= nums[i] <= 104
+ 
+
+# Follow up: If you have figured out the O(n) solution, try coding another solution of which the time complexity is O(n log(n)).
+
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        n = len(nums)
+        min_len = float('inf')
+        start = 0
+        current_sum = 0
+
+        for end in range(n):
+            current_sum += nums[end]
+            while current_sum >= target:
+                min_len = min(min_len, end - start + 1)
+                current_sum -= nums[start]
+                start += 1
+        
+        return 0 if min_len == float('inf') else min_len
